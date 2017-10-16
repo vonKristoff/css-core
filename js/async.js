f02a2c1a485236644d5450dc0564f168f80cdad0
vonKristoff <mrjcnicolas@gmail.com>
2017-10-16 14:13:46 +0100

    use stylus units to coerce numbers to pixels

--- js/async.js
+++ js/async.js
@@ -1,5 +1,6 @@
 import fg from 'fg-loadcss'
 window.onload = function() {
-    fg.loadCSS("/css/style.css")
+    console.log('aync')
+    fg.loadCSS("/style.css")
     fg.loadCSS("//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css")
 }
